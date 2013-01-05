c5ff90da2b55f4f6c191f3474742bf205e65cae8
Julien LE JEUNE <mail.julien.le.jeune@gmail.com>
2013-01-05 19:06:36 +0100

    Permit to get epg for all streams

--- jarvis/flask/views/epg.py
+++ jarvis/flask/views/epg.py
@@ -13,22 +13,34 @@ logger = logging.getLogger('jarvis-server.epg')
 
 ### GET methods ###
 @epg.route('/epg/<stream>', methods=['GET'])
+@epg.route('/epg', methods=['GET'])
 def get_epg(stream=None):
     """
     Get epg from given stream or get full epg if no one stream is given
     @param stream    : stream
     """
-    # Encode in utf8 given param (it's in unicode)
-    stream = stream.encode('utf8', "ignore")
-    if stream not in EPG_URLS:
-        return 'Your given stream %s is not defined in  [%s]' %(stream,
+    if stream != None and stream.encode('utf8', "ignore") not in EPG_URLS:
+        return 'Your given stream %s is not defined in  [%s]' % (stream,
                ', '.join(EPG_URLS.keys())), 500
-    try:
-        get = httpGET(EPG_URLS[stream])
-        epg = get.return_epg()
-    except Exception, err:
-        logger.exception(err)
-        return str(err), 500 
+    elif stream == None:
+        epg = list()
+        for stream in EPG_URLS:
+            try:
+                get = httpGET(EPG_URLS[stream])
+                info = get.return_epg()
+                epg.append(info)
+            except Exception, err:
+                logger.exception(err)
+                return str(err), 500
+    else:
+        # Encode in utf8 given param (it's in unicode)
+        stream = stream.encode('utf8', "ignore")
+        try:
+            get = httpGET(EPG_URLS[stream])
+            epg = get.return_epg()
+        except Exception, err:
+            logger.exception(err)
+            return str(err), 500
 
     return json.dumps(epg, ensure_ascii=False)
 
