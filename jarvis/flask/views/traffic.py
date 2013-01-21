d7016b90d120d9b894787127d96623e5ee216b34
Julien LE JEUNE <mail.julien.le.jeune@gmail.com>
2013-01-21 11:29:06 +0100

    Use RSS now to get traffic

--- jarvis/flask/views/traffic.py
+++ jarvis/flask/views/traffic.py
@@ -3,7 +3,7 @@
 
 import logging
 from flask import Blueprint, json, Response
-from jarvis.actions.httpGET import httpGET
+from jarvis.actions.rssRead import Feed
 
 traffic = Blueprint('traffic', __name__)
 
@@ -14,16 +14,16 @@ logger = logging.getLogger('jarvis-server.traffic')
 @traffic.route('/traffic/<train>', methods=['GET'])
 def get_traffic(train):
     """
-    Get traffic for given train (between : A, B, C, D, E ?)
+    Get traffic for given train (between : A, B, C, D, E, H, J, K, L, N, P, R, U)
     @param train : letter 
     """
     valid_trains = ('A', 'B', 'C', 'D', 'E', 'H', 'J', 'K', 'L', 'N', 'P', 'R', 'U')
     if train in valid_trains:
-        root_website = 'http://www.transilien.com/trafic/detailtrafictravaux/init?categorie=trafic&codeLigne='
+        root_website = 'http://www.transilien.com/flux/rss/traficLigne?codeLigne='
         website = root_website + train
-        # Init httpGET object with given website
-        get = httpGET(website)
-        dict_traffic = {train: get.return_traffic()}
+        # Init Feed object with given website
+        feed = Feed(website)
+        dict_traffic = {train: feed.body()}
         return Response(json.dumps(dict_traffic, ensure_ascii=False),
                         content_type='application/json; charset=utf-8')
     else:
