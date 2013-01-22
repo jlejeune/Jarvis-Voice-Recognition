14d268e5388b5c47285785c1d461bbbea86c3668
Julien LE JEUNE <mail.julien.le.jeune@gmail.com>
2013-01-22 10:48:43 +0100

    Add play_sound() function to play mp3 files

--- jarvis/flask/views/tts.py
+++ jarvis/flask/views/tts.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # vim:set ai et sts=4 sw=4:
 
+import os
 import logging
 from flask import Blueprint
 from jarvis.synthesizer import synthesizer
@@ -12,10 +13,22 @@ logger = logging.getLogger('jarvis-server.tts')
 
 ### GET methods ###
 @tts.route('/tts/<text>', methods=['GET'])
-def get_epg(text):
+def speak(text):
     """
     Speech given text
     @param text : string
     """
     synthesizer().say(text, 'fr')
     return 'OK'
+
+@tts.route('/tts/play/<path:filename>', methods=['GET'])
+def play_sound(filename):
+    """
+    Play given mp3 file
+    @param filename : mp3 file you want to play (without first /)
+    """
+    if os.path.exists('/' + filename):
+        synthesizer().play('/' + filename)
+        return 'OK'
+    else:
+        return 'Given filename /%s does not exist' % filename, 400
